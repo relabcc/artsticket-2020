cc05551e1435db6b6811d751a4ff522fd95d7234
Renddi Yeh <renddi@relab.cc>
2020-05-11 19:13:39 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.17877fa185ad4449f733798bddcf7821.js"
+  "./precache-manifest.fe1b42b9dcc3d4ec5fe60a68274516ca.js"
 );
 
 self.addEventListener('message', (event) => {
